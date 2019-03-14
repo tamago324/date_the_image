ddc02147c5f2625be69e296d356368aa6f29d804
tamago324 <tamago_pad@yahoo.co.jp>
2019-03-14 09:23:27 +0900

    init date_the_image

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, abort, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CAHNNEL_SECRET'])
+
+
+@app.route("/")
+def hello_world():
+    return "hello world!"
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers["X-Line-Signature"]
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token, TextSendMessage(text=event.message.text)
+    )
+
+
+if __name__ == "__main__":
+    app.run()
