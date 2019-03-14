11857499bdfca46fb494b8ad9aa4e609ceb668b8
tamago324 <tamago_pad@yahoo.co.jp>
2019-03-14 09:57:18 +0900

    Fix portの取得を環境変数からにした

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
+import os
+
 from flask import Flask, abort, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CAHNNEL_SECRET'])
+line_bot_api = LineBotApi(os.environ["CHANNEL_ACCESS_TOKEN"])
+handler = WebhookHandler(os.environ["CAHNNEL_SECRET"])
 
 
 @app.route("/")
 def hello_world():
+    print(os.environ["CHANNEL_ACCESS_TOKEN"])
+    print(os.environ["CAHNNEL_SECRET"])
     return "hello world!"
 
 
@@ -41,4 +44,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
